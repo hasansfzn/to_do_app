45f3a85ea2a4c108e021dbad53f12339bb1c8b0e
Softzino <softzino@Hasans-Machine.local>
2023-05-02 17:32:54 +0600

    single task component added for working

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -7,13 +7,24 @@ defineProps({
 
 <template>
   <li>
-    <label :class="[{ 'line-through text-gray-300 ': task.completed }]">
-      <input
-        type="checkbox"
-        v-model="task.completed"
-        class="checked:bg-blue-500"
-      />
-      <span class="ms-2"> {{ task.label }}</span>
+    <label
+      :class="[{ 'line-through text-gray-300 ': task.completed }]"
+      class="flex justify-between items-center"
+    >
+      <div>
+        <input
+          type="checkbox"
+          v-model="task.completed"
+          class="checked:bg-blue-500"
+        />
+        <span class="ms-2"> {{ task.label }}</span>
+      </div>
+      <button
+        class="ms-2 mt-1 bg-rose-700 hover:bg-rose-500 text-gray-700 font-semibold py-1 px-2 border border-gray-400 rounded-lg shadow"
+        @click="deleteTask"
+      >
+        X
+      </button>
     </label>
   </li>
 </template>
