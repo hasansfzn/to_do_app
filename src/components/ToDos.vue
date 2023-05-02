8ca1f4e41336c025294fea541478d1e4ce98e705
Softzino <softzino@Hasans-Machine.local>
2023-05-02 15:01:58 +0600

    ToDoList added as component

--- src/components/ToDos.vue
+++ src/components/ToDos.vue
@@ -1,18 +1,32 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import ToDoList from "./ToDoList.vue";
+
 const toDos = ref([
   { id: 1, label: "Read Vue Docs", completed: false },
   { id: 2, label: "Learn Reactivity", completed: false },
   { id: 3, label: "Learn Ref and Js Proxy", completed: false },
   { id: 4, label: "Practice Dummy Project", completed: false },
 ]);
+
+const completed = computed(() => {
+  return toDos.filter((todo) => todo.completed);
+});
+
+const inCompleted = computed(() => {
+  return toDos.fileter((todo) => !todo.completed);
+});
 </script>
 
 <template>
   <section>
     <h2 class="font-bold text-3xl text-white drop-shadow-lg shadow-black">
-      To Do Application <span></span>
+      To Do Application
     </h2>
+    <div class="grid grid-cols-2 gap-4 mt-4 pt-2">
+      <ToDoList :assignments="inCompleted" />
+      <ToDoList :assignments="completed" />
+    </div>
   </section>
 </template>
 
