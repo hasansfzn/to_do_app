f99b86c0bc95d7809a399fba499e05e684eeac33
Softzino <softzino@Hasans-Machine.local>
2023-05-02 14:51:51 +0600

    apps initated with components addition

---
+++ src/components/ToDos.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue";
+const toDos = ref([
+  { id: 1, label: "Read Vue Docs", completed: false },
+  { id: 2, label: "Learn Reactivity", completed: false },
+  { id: 3, label: "Learn Ref and Js Proxy", completed: false },
+  { id: 4, label: "Practice Dummy Project", completed: false },
+]);
+</script>
+
+<template>
+  <section>
+    <h2 class="font-bold text-3xl text-white drop-shadow-lg shadow-black">
+      To Do Application <span></span>
+    </h2>
+  </section>
+</template>
+
+<style></style>
