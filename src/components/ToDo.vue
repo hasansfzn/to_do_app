a6be82baffdac8db308b5e889c08f34198c0b94c
Softzino <softzino@Hasans-Machine.local>
2023-05-03 17:59:42 +0600

    edit functionallity intiated with addition of edit button

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { defineProps, computed, vModelCheckbox } from "vue";
+import { defineProps, computed, vModelCheckbox, ref } from "vue";
+import editImg from "../assets/images/edit.svg";
 const props = defineProps({
   task: Object,
 });
@@ -7,6 +8,12 @@ const props = defineProps({
 const deleteTask = () => {
   const res = confirm("Are you sure to delete the task?");
   if (res) props.task.deleted = true;
+};
+
+const editing = ref(false);
+
+const editName = () => {
+  editing.value = !editing.value;
 };
 </script>
 
@@ -24,7 +31,14 @@ const deleteTask = () => {
           class="checked:bg-blue-500"
         />
         <span class="ms-2" :class="[{ 'text-rose-500': task.deleted }]">
-          {{ task.label }}
+          <input
+            type="text"
+            :placeholder="task.label"
+            class="bg-transparent"
+            :disabled="!editing"
+            :autofocus="editing"
+            v-model="task.label"
+          />
         </span>
       </div>
 
@@ -32,12 +46,18 @@ const deleteTask = () => {
         v-show="!task.deleted"
         :class="[
           {
-            'ms-2 mt-1 bg-rose-300 hover:bg-rose-400 text-gray-700 font-semibold py-1 px-2 border border-gray-400 rounded-lg shadow': true,
+            'ms-2 mt-1 bg-rose-300 hover:bg-rose-400 text-gray-700 font-semibold py-1 px-2 border border-gray-400 rounded-lg shadow text-s': true,
           },
         ]"
         @click="deleteTask"
       >
         X
+      </button>
+      <button
+        class="ms-1 bg-blue-300 hover:bg-blue-400 px-2 py-2 mt-1 border border-gray-400 rounded-lg shadow"
+        @click="editName"
+      >
+        <img :src="editImg" class="h-3 w-3.5" alt="other_add" />
       </button>
     </label>
   </li>
