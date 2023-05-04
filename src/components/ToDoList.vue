8fe90442ac608ae71941c641844aa0ef88203782
Softzino <softzino@Hasans-Machine.local>
2023-05-04 15:05:05 +0600

    background changed for the colums

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -9,7 +9,7 @@ defineProps({
 </script>
 
 <template>
-  <section class="bg-cyan-500 shadow-lg shadow-cyan-500/50 p-3 rounded-lg">
+  <section class="bg-slate-100 shadow-lg shadow-gray-500/50 p-3 rounded-lg">
     <h2 class="my-2 pb-3 font-bold text-gray-800 text-center">{{ title }}</h2>
     <p v-show="!tasks.length" class="text-gray-700 text-semibold text-center">
       No tasks to show
