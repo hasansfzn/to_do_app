38a91146d9e12d694689d4b31ebe8e9d8083ac68
Softzino <softzino@Hasans-Machine.local>
2023-05-04 11:31:57 +0600

    edit input field focus added by implementing nextTick()

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,20 +1,28 @@
 <script setup>
-import { defineProps, computed, vModelCheckbox, ref } from "vue";
+import { defineProps, ref, nextTick, computed } from "vue";
 import editImg from "../assets/images/edit.svg";
 const props = defineProps({
   task: Object,
 });
+
+const editing = ref(false);
+const editInput = ref(null);
 
 const deleteTask = () => {
   const res = confirm("Are you sure to delete the task?");
   if (res) props.task.deleted = true;
 };
 
-const editing = ref(false);
-
-const editName = () => {
-  editing.value = !editing.value;
+const handleEdit = () => {
+  editing.value = true;
+  nextTick(() => {
+    editInput.value.focus();
+  });
 };
+
+const textLength = computed(() => {
+  return props.task.label.length * 9;
+});
 </script>
 
 <template>
@@ -33,37 +41,40 @@ const editName = () => {
         />
         <!-- task name  -->
         <span class="ms-2" :class="[{ 'text-rose-500': task.deleted }]">
-          {{ editing }}
           <input
+            ref="editInput"
             type="text"
             :placeholder="task.label"
-            class="bg-transparent focus:outline-gray-300 border-none"
+            class="bg-transparent focus:outline-gray-300"
+            :style="{ width: textLength + 'px' }"
             :disabled="!editing"
-            autofocus
             v-model="task.label"
+            @keydown.enter="editing = false"
+            @focusout="editing = false"
           />
         </span>
       </div>
       <!-- action button  -->
+      <div>
+        <button
+          v-show="!task.deleted"
+          :class="[
+            {
+              'ms-2 mt-1 bg-rose-300 hover:bg-rose-400 text-gray-700 font-semibold py-1 px-2 border border-gray-400 rounded-lg shadow text-s': true,
+            },
+          ]"
+          @click="deleteTask"
+        >
+          X
+        </button>
 
-      <button
-        v-show="!task.deleted"
-        :class="[
-          {
-            'ms-2 mt-1 bg-rose-300 hover:bg-rose-400 text-gray-700 font-semibold py-1 px-2 border border-gray-400 rounded-lg shadow text-s': true,
-          },
-        ]"
-        @click="deleteTask"
-      >
-        X
-      </button>
-
-      <button
-        class="ms-1 bg-blue-300 hover:bg-blue-400 px-2 py-2 mt-1 border border-gray-400 rounded-lg shadow"
-        @click="editName"
-      >
-        <img :src="editImg" class="h-3 w-3.5" alt="other_add" />
-      </button>
+        <button
+          class="ms-1 bg-blue-300 hover:bg-blue-400 px-2 py-2 mt-1 border border-gray-400 rounded-lg shadow"
+          @click="handleEdit"
+        >
+          <img :src="editImg" class="h-3 w-3.5" alt="other_add" />
+        </button>
+      </div>
     </label>
   </li>
 </template>
