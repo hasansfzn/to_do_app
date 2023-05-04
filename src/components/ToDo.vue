d44350f4dacbfe665953e2e0015b6fdab2be95f7
Softzino <softzino@Hasans-Machine.local>
2023-05-04 12:40:03 +0600

    toDo.vue file refactored for edit

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -26,26 +26,28 @@ const textLength = computed(() => {
 </script>
 
 <template>
-  <li>
-    <label class="flex justify-between items-center">
-      <div>
-        <!-- checkbox  -->
-        <input
-          v-show="!task.deleted"
-          type="checkbox"
-          v-model="task.completed"
-          class="checked:bg-blue-500"
-        />
-        <!-- task name  -->
-        <span class="ms-2" :class="[{ 'text-rose-500': task.deleted }]">
+  <li class="flex justify-between items-center">
+    <div>
+      <!-- checkbox  -->
+      <input
+        :id="'taskName' + task.id"
+        v-show="!task.deleted"
+        type="checkbox"
+        v-model="task.completed"
+        class="checked:bg-blue-500"
+      />
+      <label :for="'taskName' + task.id">
+        <span class="p-1">
+          <!-- task name  -->
           <input
             :style="{ width: textLength + 'px' }"
             ref="editInput"
             type="text"
             :placeholder="task.label"
             :class="[
+              { 'text-rose-500': task.deleted },
               { 'line-through text-gray-300 ': task.completed },
-              { 'bg-transparent focus:outline-gray-300': true },
+              { 'bg-transparent focus:outline-gray-300 ms-2': true },
             ]"
             :disabled="!editing"
             v-model="task.label"
@@ -53,30 +55,30 @@ const textLength = computed(() => {
             @focusout="editing = false"
           />
         </span>
-      </div>
-      <!-- action button  -->
-      <div>
-        <button
-          v-show="!task.deleted"
-          :class="[
-            {
-              'ms-2 mt-1 bg-rose-300 hover:bg-rose-400 text-gray-700 font-semibold py-1 px-2 border border-gray-400 rounded-lg shadow text-s': true,
-            },
-          ]"
-          @click="deleteTask"
-        >
-          X
-        </button>
+      </label>
+    </div>
+    <!-- action button  -->
+    <div>
+      <button
+        v-show="!task.deleted"
+        :class="[
+          {
+            'ms-2 mt-1 bg-rose-300 hover:bg-rose-400 text-gray-700 font-semibold py-1 px-2 border border-gray-400 rounded-lg shadow text-s': true,
+          },
+        ]"
+        @click="deleteTask"
+      >
+        X
+      </button>
 
-        <button
-          v-show="!task.deleted"
-          class="ms-1 bg-blue-300 hover:bg-blue-400 px-2 py-2 mt-1 border border-gray-400 rounded-lg shadow"
-          @click="handleEdit"
-        >
-          <img :src="editImg" class="h-3 w-3.5" alt="other_add" />
-        </button>
-      </div>
-    </label>
+      <button
+        v-show="!task.deleted"
+        class="ms-1 bg-blue-300 hover:bg-blue-400 px-2 py-2 mt-1 border border-gray-400 rounded-lg shadow"
+        @click="handleEdit"
+      >
+        <img :src="editImg" class="h-3 w-3.5" alt="other_add" />
+      </button>
+    </div>
   </li>
 </template>
 
